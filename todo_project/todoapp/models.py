1230566ea3edd7c687f019ddf4124062eef979f8
jkinobe <jkinobe@gmail.com>
2020-06-22 15:34:40 +0300

    models

--- todo_project/todoapp/models.py
+++ todo_project/todoapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100) 
+    class Meta:
+        verbose_name = ("Category")
+        verbose_name_plural = ("Categories")
+    def __str__(self):
+        return self.name #name to be shown in admin dashboard
